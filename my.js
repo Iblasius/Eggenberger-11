085fa3963ab806a7193aa33fac959fdb23aafbda
Iblasius <juergen.ebenberger@gmail.com>
2014-07-31 15:47:50 +0200

    GetPic

--- my.js
+++ my.js
@@ -19,14 +19,33 @@ function DOMLoaded(){
 
 function phonegapLoaded(){
 	
-	//$(function() {
-	//	
-	//	$("body").append("<h1>DEVICE is ready!</h1>");
-	//});
-	navigator.notification.alert(
-    'You are the winner!',  // message
-    alertDismissed,         // callback
-    'Game Over',            // title
-    'Done'                  // buttonName
-	);
+	$(function() {
+		
+		$("body").append("<h1>DEVICE is ready!</h1>");
+		$("body").append("<button class='ui-button' onClick='getPic()'></button>");
+	});
+	//navigator.notification.alert(
+    //'You are the winner!',  // message
+    //alertDismissed,         // callback
+    //'Game Over',            // title
+    //'Done'                  // buttonName
+	//);
+}
+
+function getPic(){
+
+	navigator.camera.getPicture(onSuccess, onFail, {
+		quality: 40,
+		destinationType: Camera.DestinationType.FILE_URI,
+		targetWidth: 400,
+		targetHeight: 400
+		)};
+}
+
+function onSuccess(imageURI){
+	$("body").append("<img src='" + imageURI + "'>");
+}
+
+function onFail(message){
+	alert("Das Foto konnte nicht geladen werden: " + message);
 }
