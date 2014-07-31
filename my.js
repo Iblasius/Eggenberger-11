211617ac4bc82b60673799b9e10ea7b989bc03a5
Iblasius <juergen.ebenberger@gmail.com>
2014-07-31 14:59:06 +0200

    myConfig

--- my.js
+++ my.js
@@ -1,12 +1,25 @@
 function DOMLoaded(){
 	
+	//var myinterval = window.setInterval(
+	//
+	//	function() {
+	//		if(PhoneGap.available){
+	//			window.clearInterval(myinterval);
+	//			phonegapLoaded();
+	//		}
+	//	}, 500
+	//);
 	document.addEventListener("deviceready", phonegapLoaded, false);
 }
 
 function phonegapLoaded(){
 	
-	$(function() {
-		
-		$("body").append("<h1>DEVICE is ready!</h1>");
-	});
+	//$(function() {
+	//	
+	//	$("body").append("<h1>DEVICE is ready!</h1>");
+	//});
+	navigator.notification.alert(
+		'Hello World',
+		dismissed,
+	);
 }
